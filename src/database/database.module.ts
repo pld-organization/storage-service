c07509e627c3be398e8f6d98084147f800163e2a
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-04-26 22:41:48 +0200

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,9 +6,11 @@ import { MongoClient } from 'mongodb';
     {
       provide: 'MONGO_DB',
       useFactory: async () => {
-        const client = new MongoClient('mongodb://127.0.0.1:27017');
+        const uri = process.env.MONGODB_URI; 
+        const client = new MongoClient(uri);
         await client.connect();
-        return client.db('clinic_db'); // your DB name
+        const dbName = process.env.MONGODB_DB || 'clinic_db';
+        return client.db(dbName);
       },
     },
   ],
