5f6a72d340444786bc763ed12dc998280ebd336c
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-04-29 23:59:46 +0100

    Refactor AppModule to include AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,27 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UploadModule } from './uplaoder/upload.module';
-
-
 import mongoConfig from './database/database.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-
     ConfigModule.forRoot({
       isGlobal: true,
       load: [mongoConfig],
     }),
-
-
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('mongodb.uri'),
-        dbName: configService.get<string>('mongodb.database'),
+        uri: configService.get('mongodb.uri'),
+        dbName: configService.get('mongodb.database'),
       }),
     }),
-
-
     UploadModule,
   ],
+  controllers: [AppController], 
+  providers: [AppService],      
 })
 export class AppModule {}
