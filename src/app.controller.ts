0c04528069a2714f2fa20aa6fc683af963c532b6
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-04-30 00:32:14 +0100

    Log instantiation of AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  constructor() {
+    console.log('✅ AppController instantiated');
+  }
   @Get('health')
   health() {
     return { status: 'ok' };
